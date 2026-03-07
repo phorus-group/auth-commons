b37ebb741b5c9608493fdc080cd64dd0f098766d
Martin Rios <irios@phorus.group>
2026-03-07 13:19:38 +0200

    Update exception-handling dependency to 1.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ ext["jackson-bom.version"] = "3.1.0"
 
 group = "group.phorus"
 description = "Library containing common AuthN logic for Spring based services."
-version = "2.0.1"
+version = "2.0.2"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -50,7 +50,7 @@ dependencies {
 
     // Phorus
     implementation("group.phorus:mapper:1.2.0")
-    implementation("group.phorus:exception-handling:1.1.1")
+    implementation("group.phorus:exception-handling:1.1.2")
 
     // JWT
     api("org.bouncycastle:bcprov-jdk18on:1.80")
